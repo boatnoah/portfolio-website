115c3bdbabfaaebfbd204061221b01cbc96a4d16
boatnoah <noahskim7@gmail.com>
2024-01-24 21:01:31 -0800

    react and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./componenets/NavBar.tsx";
+
+function App() {
+  return (
+    <>
+      <div>
+        <NavBar />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/componenets/NavBar.tsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <>
+      <div>
+        <h3>Noah Kim</h3>
+        <a>Projects</a>
+        <a>top</a>
+        <a>Socials</a>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
